bef3fb9bfa2f18edfa691d48fa98ee4fc74cb810
Kathleen Shea <shea9@ruby966.llnl.gov>
2023-12-06 10:21:25 -0800

    fixed the sdl errors with #ifdefs

--- nanopond.c
+++ nanopond.c
@@ -252,7 +252,7 @@
 /* Define this to use SDL. To use SDL, you must have SDL headers
  * available and you must link with the SDL library when you compile. */
 /* Comment this out to compile without SDL visualization support. */
-#define USE_SDL 1
+// #define USE_SDL 1
 
 /* Define this to use threads, and how many threads to create */
 #define USE_PTHREADS_COUNT 4
@@ -350,11 +350,11 @@ static struct Cell pond[POND_SIZE_X][POND_SIZE_Y];
 static volatile uint64_t cellIdCounter = 0;
 
 /* Currently selected color scheme */
-enum { KINSHIP,LINEAGE,MAX_COLOR_SCHEME } colorScheme = KINSHIP;
-static const char *colorSchemeName[2] = { "KINSHIP", "LINEAGE" };
 
 #ifdef USE_SDL
 static SDL_Window *window;
+enum { KINSHIP,LINEAGE,MAX_COLOR_SCHEME } colorScheme = KINSHIP;
+static const char *colorSchemeName[2] = { "KINSHIP", "LINEAGE" };
 static SDL_Surface *winsurf;
 static SDL_Surface *screen;
 #endif
@@ -446,6 +446,7 @@ static void doReport(const uint64_t clock)
  * @param file Destination
  * @param cell Source
  */
+#ifdef USE_SDL
 static void dumpCell(FILE *file, struct Cell *cell)
 {
 	uintptr_t wordPtr,shiftPtr,inst,stopCount,i;
@@ -477,7 +478,7 @@ static void dumpCell(FILE *file, struct Cell *cell)
 	}
 	fprintf(file,"\n");
 }
-
+#endif
 static inline struct Cell *getNeighbor(const uintptr_t x,const uintptr_t y,const uintptr_t dir)
 {
 	/* Space is toroidal; it wraps at edges */
@@ -501,7 +502,7 @@ static inline int accessAllowed(struct Cell *const c2,const uintptr_t c1guess,in
 	 * "negative" interactions and sense 1 for "positive" ones. */
 	return sense ? (((getRandom() & 0xf) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID)) : (((getRandom() & 0xf) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID));
 }
-
+#ifdef USE_SDL
 static inline uint8_t getColor(struct Cell *c)
 {
 	uintptr_t i,j,word,sum,opcode,skipnext;
@@ -557,6 +558,7 @@ static inline uint8_t getColor(struct Cell *c)
 	}
 	return 0; /* Cells with no energy are black */
 }
+#endif
 
 volatile int exitNow = 0;
 
