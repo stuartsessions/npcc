c0417cb1d9365e6cb6880519de23c486abe29861
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-07 10:42:37 -0800

    added bin

--- pack.c
+++ pack.c
@@ -59,58 +59,11 @@ struct Cell
 	 * bit instructions packed into machine size words) */
 	uintptr_t genome[POND_DEPTH_SYSWORDS];
 };
-// A utility function to reverse a string
-void reverse(char str[], int length)
+void bin(unsigned n)
 {
-    int start = 0;
-    int end = length - 1;
-    while (start < end) {
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        end--;
-        start++;
-    }
-}
-// Implementation of citoa()
-char* itoa(int num, char* str, int base)
-{
-    int i = 0;
-    bool isNegative = false;
- 
-    /* Handle 0 explicitly, otherwise empty string is
-     * printed for 0 */
-    if (num == 0) {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
-    }
- 
-    // In standard itoa(), negative numbers are handled
-    // only with base 10. Otherwise numbers are
-    // considered unsigned.
-    if (num < 0 && base == 10) {
-        isNegative = true;
-        num = -num;
-    }
- 
-    // Process individual digits
-    while (num != 0) {
-        int rem = num % base;
-        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-        num = num / base;
-    }
- 
-    // If number is negative, append '-'
-    if (isNegative)
-        str[i++] = '-';
- 
-    str[i] = '\0'; // Append string terminator
- 
-    // Reverse the string
-    reverse(str, i);
- 
-    return str;
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
 }
 
 /* The pond is a 2D array of cells */
@@ -160,8 +113,7 @@ static void writeCell(FILE *file, struct Cell *cell) {
 			 * a LOOP/REP pair that's always false. In any case, this
 			 * would always result in our *underestimating* the size of
 			 * the genome and would never result in an overestimation. */
-            char buffer [33];
-            itoa(inst, buffer, 2);
+            buffer = bin(inst)
             fprintf(file, "%s", buffer);
 			if (inst == 0xf) { /* STOP */
 				if (++stopCount >= 4)
