0458744c8e86c0c8ac1565863d9e4a6ee7a79892
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-07 15:15:00 -0800

    added printing

--- pack.c
+++ pack.c
@@ -1,4 +1,13 @@
-
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c\n"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
 
 
 /* Depth of pond in four-bit codons -- this is the maximum
@@ -157,10 +166,10 @@ int main(int argc, char** argv) {
         printf("Failed to create the file.\n");
         return 1;
     }
-   // for(unsigned int i=0;i<POND_DEPTH_SYSWORDS;++i){
-        fprintf(file1, "%x", (unsigned int)pond[0][0].genome[i]);
-    //}
-    fwrite(pond[0][0].genome, sizeof(pond[0][0].genome), 1, file1);
+    for(unsigned int i=0;i<POND_DEPTH_SYSWORDS;++i){
+        fprintf(file1, BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY((unsigned int)pond[0][0].genome[i]));
+    }
+    //fwrite(pond[0][0].genome, sizeof(pond[0][0].genome), 1, file1);
     //writeCell(file1, &c1);
     
     return 0;
