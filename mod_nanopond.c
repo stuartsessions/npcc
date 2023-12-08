871f41534382a7788c1ba93f1fec32c83aff82d8
Stuart Sessions <sessions@ruby961.llnl.gov>
2023-12-08 13:46:55 -0800

    commented out broken accessAllowed change

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -500,7 +500,23 @@ static inline int accessAllowed(struct Cell *const c2,const uintptr_t c1guess,in
 	/* Access permission is more probable if they are more similar in sense 0,
 	 * and more probable if they are different in sense 1. Sense 0 is used for
 	 * "negative" interactions and sense 1 for "positive" ones. */
-	return sense ? (((getRandom() & 0xf) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID)) : (((getRandom() & 0xf) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID));
+/*	
+	return !!sense * (
+            (
+             (getRandom() & 0xf) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) 
+             ^ 
+             (c1guess & 0xf)])
+            ||(!c2->parentID)
+            ) 
+        +
+       !sense * (
+                ((getRandom() & 0xf) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])
+                ||(!c2->parentID));
+
+  */
+    
+    return sense ? (((getRandom() & 0xf) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID)) : (((getRandom() & 0xf) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID));
+
 }
 #ifdef USE_SDL
 static inline uint8_t getColor(struct Cell *c)
