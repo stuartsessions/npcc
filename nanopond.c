0b4c9769b9a43ad953fd8df3a246727d96faae7f
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-10 11:00:11 -0800

    added a working readCell method which takes a string of 1s and 0's and reads them into a cell's genome

--- nanopond.c
+++ nanopond.c
@@ -439,7 +439,41 @@ static void doReport(const uint64_t clock)
 	for(x=0;x<sizeof(statCounters);++x)
 		((uint8_t *)&statCounters)[x] = (uint8_t)0;
 }
+/**
+ * Reads the contents of the genome to the file
+ *
+ * @param genomeData The genome data in string format
+ * @return The a cell instantiated with the genome data
+ */
+static struct Cell readCell(char *genomeData) {
+    uintptr_t wordPtr = 0;
+    uintptr_t shiftPtr = 0;
+    uintptr_t packedValue = 0;
+    struct Cell cell;
 
+    for (int i = 0; genomeData[i] != '\0'; i++) {
+        char character = genomeData[i];
+        if (character >= '0' && character <= 'f') { // checks if the character is valid hex character
+            uintptr_t value = character <= '9' ? character - '0' : character - 'a' + 10; // this is a line to convert hex to decimal value. 
+            // if the character is between 0 and 9, use character - '0' to get the decimal value.
+            // else if the character is not between 0 and 9, use character - 'a' + 10 to get the decimal value
+            packedValue |= value << shiftPtr; // shift the value to the left by shiftPtr and OR it with packedValue
+            shiftPtr += 4; // increment shiftPtr by 4
+
+            if (shiftPtr >= SYSWORD_BITS) { //if shiftPtr is greater that SYSWORD_BITS, then we have a full word and need to store it in the genome
+                cell.genome[wordPtr] = packedValue;
+                wordPtr++; // increment wordPtr so we know where to put it in the array
+                shiftPtr = 0; //reset shiftPtr to 0
+                packedValue = 0; //reset packedValue to 0
+            }
+        }
+    }
+
+    if (shiftPtr > 0) {
+        cell.genome[wordPtr] = packedValue; // store the last word (overflow < SYSWORD_BITS)
+    }
+    return cell;
+}
 /**
  * Dumps the genome of a cell to a file.
  *
