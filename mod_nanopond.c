937c5951d5e931ca2a0222a7b8a6d3e26bcdd0b2
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-11 15:21:02 -0800

    added stop 2

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -676,7 +676,7 @@ static void *run(void *targ)
 				* set in 0x9 & 0xf
 				*/
 				stop=
-				(inst = 0x0 || inst == 0x1 || inst == 0x2 || inst == 0x3 || inst == 0x4 || inst == 0x5 || inst == 0x6 || inst == 0x7 || inst == 0x8 || inst == 0xa || inst == 0xb || inst == 0xc || inst == 0xd || inst == 0xe)*(stop)+
+				(inst == 0x0 || inst == 0x1 || inst == 0x2 || inst == 0x3 || inst == 0x4 || inst == 0x5 || inst == 0x6 || inst == 0x7 || inst == 0x8 || inst == 0xa || inst == 0xb || inst == 0xc || inst == 0xd || inst == 0xe)*(stop)+
 				((inst == 0x9)*(stop*!(reg&&(loopStackPtr>=POND_DEPTH))+(reg&&(loopStackPtr>=POND_DEPTH))))+
 				((inst == 0xf)*(1));
 
