3244137c379e2f18bc3dae2c804e94a1b5f88cc2
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-11 10:40:34 -0800

    debugging line by line 0xc

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -806,7 +806,7 @@ static void *run(void *targ)
 				((inst==0x4)*((reg - 1) & 0xf)) + 
 				((inst==0x5)*((pptr->genome[ptr_wordPtr] >> ptr_shiftPtr) & 0xf)) +
 				((inst==0x7)*((outputBuf[ptr_wordPtr] >> ptr_shiftPtr) & 0xf)) + 
-				((inst==0xc)*((pptr->genome[wordPtr] >> shiftPtr) & 0xf));
+				//((inst==0xc)*((pptr->genome[wordPtr] >> shiftPtr) & 0xf));
 				/*facing is called in 0x0 and 0xb
 				* facing is used to determine which direction the cell is facing
 				*/
@@ -975,7 +975,7 @@ static void *run(void *targ)
                         shiftPtr=(shiftPtr+4)+(shiftPtr+4>=SYSWORD_BITS)*(-shiftPtr-4);
  
 						tmp = reg;
-						//reg = (pptr->genome[wordPtr] >> shiftPtr) & 0xf;
+						reg = (pptr->genome[wordPtr] >> shiftPtr) & 0xf;
 						pptr->genome[wordPtr]=((pptr->genome[wordPtr]&~(((uintptr_t)0xf) << shiftPtr))|tmp << shiftPtr);
 						currentWord = pptr->genome[wordPtr];
 						break;
