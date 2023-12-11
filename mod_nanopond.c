e5322502133dda23cc2645d0fe60646cd4a55c8a
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-11 10:15:14 -0800

    added reg back in

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -798,12 +798,12 @@ static void *run(void *targ)
 				* reg is called in 0x0, 0x3, 0x4, 0x5, 0x7, 0xc, 0xe
 				* 
 				*/
-				/*
+				
 				reg=
 				(inst=0x1 || inst == 0x2 || inst == 0x6 || inst == 0x8 || inst==0x9 || inst == 0xa || inst == 0xb || inst == 0xd ||inst == 0xe || inst==0xf)
-				* (reg) + ((inst==0x0)*0)+ ((inst==0x3)*(reg + 1) & 0xf) +
-				((inst==0x4)*(reg - 1) & 0xf) + ((inst==0x5)*((pptr->genome[ptr_wordPtr] >> ptr_shiftPtr) & 0xf)) +
-				((inst==0x7)*(outputBuf[ptr_wordPtr] >> ptr_shiftPtr) & 0xf) + ((inst==0xc)*(pptr->genome[wordPtr] >> shiftPtr) & 0xf) + ((inst==0xe)*(pptr->parentID));
+				* (reg) + ((inst==0x0)*0)+ ((inst==0x3)*((reg + 1) & 0xf)) +
+				((inst==0x4)*((reg - 1) & 0xf)) + ((inst==0x5)*((pptr->genome[ptr_wordPtr] >> ptr_shiftPtr) & 0xf)) +
+				((inst==0x7)*((outputBuf[ptr_wordPtr] >> ptr_shiftPtr) & 0xf)) + ((inst==0xc)*((pptr->genome[wordPtr] >> shiftPtr) & 0xf));
 				/*facing is called in 0x0 and 0xb
 				* facing is used to determine which direction the cell is facing
 				*/
@@ -827,7 +827,7 @@ static void *run(void *targ)
                 // var = (i==0x1){A} + (i==0x2){B} + ...	
 				switch(inst) {
 					case 0x0: /* ZERO: Zero VM state registers */
-						reg = 0;
+						//reg = 0;
 						ptr_wordPtr = 0;
 						ptr_shiftPtr = 0;
 						//facing = 0;
@@ -874,13 +874,13 @@ static void *run(void *targ)
                        */
                         break;
 					case 0x3: /* INC: Increment the register */
-						reg = (reg + 1) & 0xf;
+						//reg = (reg + 1) & 0xf;
 						break;
 					case 0x4: /* DEC: Decrement the register */
-						reg = (reg - 1) & 0xf;
+						//reg = (reg - 1) & 0xf;
 						break;
 					case 0x5: /* READG: Read into the register from genome */
-						reg = (pptr->genome[ptr_wordPtr] >> ptr_shiftPtr) & 0xf;
+						//reg = (pptr->genome[ptr_wordPtr] >> ptr_shiftPtr) & 0xf;
 						break;
 					case 0x6: /* WRITEG: Write out from the register to genome */
                         
@@ -891,7 +891,7 @@ static void *run(void *targ)
                         currentWord = pptr->genome[wordPtr]; /* Must refresh in case this changed! */
 						break;
 					case 0x7: /* READB: Read into the register from buffer */
-						reg = (outputBuf[ptr_wordPtr] >> ptr_shiftPtr) & 0xf;
+						//reg = (outputBuf[ptr_wordPtr] >> ptr_shiftPtr) & 0xf;
 						break;
 					case 0x8: /* WRITEB: Write out from the register to buffer */
 						outputBuf[ptr_wordPtr]=(outputBuf[ptr_wordPtr]&~(((uintptr_t)0xf) << ptr_shiftPtr))|reg << ptr_shiftPtr;
@@ -971,7 +971,7 @@ static void *run(void *targ)
                         shiftPtr=(shiftPtr+4)+(shiftPtr+4>=SYSWORD_BITS)*(-shiftPtr-4);
  
 						tmp = reg;
-						reg = (pptr->genome[wordPtr] >> shiftPtr) & 0xf;
+						//reg = (pptr->genome[wordPtr] >> shiftPtr) & 0xf;
 						pptr->genome[wordPtr]=((pptr->genome[wordPtr]&~(((uintptr_t)0xf) << shiftPtr))|tmp << shiftPtr);
 						currentWord = pptr->genome[wordPtr];
 						break;
