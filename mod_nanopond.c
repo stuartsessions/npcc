24b39bc1a20cf8a4ec97ee59bc72f70588a664bb
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-11 19:15:35 -0800

    changed getRandom()

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -293,8 +293,8 @@ void precalculate_random_numbers() {
 static inline uintptr_t getRandom() {
     uintptr_t num = buffer[in];
     last_random_number = num;  // Store the last random number
+	buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
     in = (in + 1) % BUFFER_SIZE;  // Wrap around to 0 when index reaches BUFFER_SIZE
-    buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
     return num;
 }
 /* Pond depth in machine-size words.  This is calculated from
