0c4f663289acf380bc42f0cc1bdc2574a388b4ff
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-12 11:28:15 -0800

    added back original accessAllowed

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -459,8 +459,10 @@ static inline struct Cell *getNeighbor(const uintptr_t x, const uintptr_t y, con
 
 static inline int accessAllowed(struct Cell *const c2, const uintptr_t c1guess, int sense)
 {
-    uintptr_t random = (uintptr_t)(getRandom() & 0xf);
-    return ((((random >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)]) || !c2->parentID) & sense) | (((random <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)]) || !c2->parentID) & ~sense));
+    /* Access permission is more probable if they are more similar in sense 0,
+	 * and more probable if they are different in sense 1. Sense 0 is used for
+	 * "negative" interactions and sense 1 for "positive" ones. */
+	return sense ? (((getRandom() & 0xf) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID)) : (((getRandom() & 0xf) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID));
 }
 /*
 static inline int accessAllowedSwitch(struct Cell *const c2, const uintptr_t c1guess, int sense)
