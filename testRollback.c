e8754df3e7f2f705b31ac630112c8f4848681d7b
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-12 11:46:51 -0800

    added testing file

---
+++ testRollback.c
@@ -0,0 +1,81 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define BUFFER_SIZE 1000  // Size of the circular buffer
+static uintptr_t buffer[BUFFER_SIZE];
+static int in = 0;
+static uintptr_t last_random_number;
+
+volatile uint64_t prngState[2];
+
+static inline uintptr_t getRandomPre()
+{
+	// https://en.wikipedia.org/wiki/Xorshift#xorshift.2B
+	uint64_t x = prngState[0];
+	const uint64_t y = prngState[1];
+	prngState[0] = y;
+	x ^= x << 23;
+	const uint64_t z = x ^ y ^ (x >> 17) ^ (y >> 26);
+	prngState[1] = z;
+	return (uintptr_t)(z + y);
+}
+void precalculate_random_numbers() {
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+        buffer[i] = getRandomPre();
+    }
+}
+static inline uintptr_t getRandom() {
+    uintptr_t num = buffer[in];
+    last_random_number = num;  // Store the last random number
+	buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
+    in = (in + 1) % BUFFER_SIZE;  // Wrap around to 0 when index reaches BUFFER_SIZE
+    return num;
+}
+static inline uintptr_t getRandomRollback(uintptr_t rollback) {
+    uintptr_t num = buffer[in];
+    last_random_number = num;  // Store the last random number
+    buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
+    in = ((in + 1) % BUFFER_SIZE) * rollback + in * (!rollback);  // Roll back if rollback is zero
+    return num;
+}
+
+
+int main() {
+    // Initialize the PRNG state
+    prngState[0] = time(NULL);
+    prngState[1] = 0xDEADBEEF;
+
+    // Precalculate random numbers
+    precalculate_random_numbers();
+
+    // Get a random number without rollback
+    uintptr_t num1 = getRandomRollback(1);
+    printf("Random number without rollback: %lu\n", num1);
+
+    // Get a random number with rollback
+    uintptr_t num2 = getRandomRollback(0);
+    printf("Random number with rollback: %lu\n", num2);
+
+    // Check if the random number with rollback is the same as the previous number
+    if (num2 == num1) {
+        printf("Rollback works correctly\n");
+    } else {
+        printf("Rollback does not work correctly\n");
+    }
+
+    // Get another random number without rollback
+    uintptr_t num3 = getRandomRollback(1);
+    printf("Another random number without rollback: %lu\n", num3);
+
+    // Check if the new random number is different from the previous numbers
+    if (num3 != num1 && num3 != num2) {
+        printf("Random number generation works correctly\n");
+    } else {
+        printf("Random number generation does not work correctly\n");
+    }
+
+    return 0;
+}
