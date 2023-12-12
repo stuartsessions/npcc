7f7ec91a64d5e241eed939efce2b3508506bf6f0
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-12 11:39:36 -0800

    chnaged accessAllowed

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -457,11 +457,12 @@ static inline struct Cell *getNeighbor(const uintptr_t x, const uintptr_t y, con
 
 static inline int accessAllowed(struct Cell *const c2, const uintptr_t c1guess, int sense)
 {
+	uintptr_t random = (uintptr_t)(getRandomRollback(1) & 0xf);
     /* Access permission is more probable if they are more similar in sense 0,
 	 * and more probable if they are different in sense 1. Sense 0 is used for
 	 * "negative" interactions and sense 1 for "positive" ones. */
 	//return sense ? (((getRandomRollback(1) & 0xf) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID)) : (((getRandomRollback(1) & 0xf) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)])||(!c2->parentID));
-	return ((((getRandomRollback(1) >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)]) || !c2->parentID) & sense) | (((getRandomRollback(1) <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)]) || !c2->parentID) & ~sense));
+	return ((((random >= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)]) || !c2->parentID) & sense) | (((random <= BITS_IN_FOURBIT_WORD[(c2->genome[0] & 0xf) ^ (c1guess & 0xf)]) || !c2->parentID) & ~sense));
 }
 /*
 static inline int accessAllowedSwitch(struct Cell *const c2, const uintptr_t c1guess, int sense)
