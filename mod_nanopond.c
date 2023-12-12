ac25e0f92d53dcd624c7f405cde2b2f802fce8c0
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-12 11:30:17 -0800

    changed random to mine

--- mod_nanopond.c
+++ mod_nanopond.c
@@ -271,36 +271,33 @@ static int in = 0;
 static uintptr_t last_random_number;
 
 
-volatile uint64_t prngState[2];
-static inline uintptr_t getRandomPre(int rollback)
+static inline uintptr_t getRandomPre()
 {
-    // https://en.wikipedia.org/wiki/Xorshift#xorshift.2B
-    uint64_t x = prngState[0];
-    const uint64_t y = prngState[1];
-    prngState[0] = prngState[0] * rollback + !rollback * y; 
-    x ^= x << 23;
-    const uint64_t z = x ^ y ^ (x >> 17) ^ (y >> 26); 
-    prngState[1] = prngState[1] * rollback + !rollback * z; 
-    return (uintptr_t)(z + y);
+	// https://en.wikipedia.org/wiki/Xorshift#xorshift.2B
+	uint64_t x = prngState[0];
+	const uint64_t y = prngState[1];
+	prngState[0] = y;
+	x ^= x << 23;
+	const uint64_t z = x ^ y ^ (x >> 17) ^ (y >> 26);
+	prngState[1] = z;
+	return (uintptr_t)(z + y);
 }
-
 void precalculate_random_numbers() {
     for (int i = 0; i < BUFFER_SIZE; i++) {
-        buffer[i] = getRandomPre(1);
+        buffer[i] = getRandomPre();
     }
 }
-
 static inline uintptr_t getRandom() {
     uintptr_t num = buffer[in];
     last_random_number = num;  // Store the last random number
-	buffer[in] = getRandomPre(1);  // Generate a new random number and add it to the buffer
+	buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
     in = (in + 1) % BUFFER_SIZE;  // Wrap around to 0 when index reaches BUFFER_SIZE
     return num;
 }
 static inline uintptr_t getRandomRollback(uintptr_t rollback) {
     uintptr_t num = buffer[in];
     last_random_number = num;  // Store the last random number
-    buffer[in] = getRandomPre(rollback);  // Generate a new random number and add it to the buffer
+    buffer[in] = getRandomPre();  // Generate a new random number and add it to the buffer
     in = ((in + 1) % BUFFER_SIZE) * rollback + in * (!rollback);  // Roll back if rollback is zero
     return num;
 }
